4bacd59aadc78f813f5f5212962bf8d3bda20b4a
nabiltiabi1 <89665542+nabiltiabi1@users.noreply.github.com>
2025-04-12 03:52:36 +0100

    add logo and css to separated files

--- src/app/receipt-list/receipt-list.component.ts
+++ src/app/receipt-list/receipt-list.component.ts
@@ -3,7 +3,8 @@ import { ReceiptService } from '../receipt.service';
 
 @Component({
   selector: 'app-receipt-list',
-  templateUrl: './receipt-list.component.html'
+  templateUrl: './receipt-list.component.html',
+  styleUrls: ['./receipt-list.component.css']
 })
 export class ReceiptListComponent {
   constructor(private receiptService: ReceiptService) {}
